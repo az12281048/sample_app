0074e0dfeb0b1115d8cb13a7b011d70d2b03bbbf
az12281048 <thomas544908417@hotmail.com>
2015-07-15 14:55:06 +0800

    forget to use branch to code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns teh full title on a per-page basis
+  def full_tirle(page_title)
+    base_title = "RUby on Rails Tutorial Sample App"
+    if page_title.empty?
+      bas_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
